65f17e12a9309d069eb74da2a55527c9214e1c39
lauroagronomo1976-blip <lauroagronomo1976@gmail.com>
2026-01-03 21:00:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("JS carregou");
+document.addEventListener("DOMContentLoaded", function () {
+
+  const map = L.map("map").setView([-15, -50], 5);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19
+  }).addTo(map);
+
+});
